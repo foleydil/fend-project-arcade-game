bf03f498d4e1d7f33e3f725b076536c10162d942
Dillon Foley <foleydil@gmail.com>
2018-07-22 15:16:32 -0400

    refactor: Use ES6 class for Player constructor

--- js/app.js
+++ js/app.js
@@ -24,17 +24,29 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-let player = {
-  update: player => player,
-  render: player => player,
-  handleInput: player => player
+class Player {
+  constructor(plyrImage = 'image.png') {
+    this.plyrImage = plyrImage;
+  }
+
+  update() {
+
+  }
+
+  render() {
+
+  }
+
+  handleInput() {
+
+  }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
-
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.

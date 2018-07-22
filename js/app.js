67fc7268c6cf1adbce5d52d4933164eb9fab1d60
Dillon Foley <foleydil@gmail.com>
2018-07-22 15:07:53 -0400

    feat: Instatiate player and allEnemies to render canvas

--- js/app.js
+++ js/app.js
@@ -24,12 +24,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+let player = {
+  update: player => player,
+  render: player => player,
+  handleInput: player => player
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your

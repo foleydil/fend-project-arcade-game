a8f5f07c14f2d0a54733b4227c50b0de6be0761e
Dillon Foley <foleydil@gmail.com>
2018-07-22 15:26:34 -0400

    refactor: Update Enemy to ES6 class constructor

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+  constructor() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -46,6 +50,9 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
+for (var i = 0; i < 3; i++) {
+  allEnemies.push(new Enemy());
+}
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
